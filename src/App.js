1a0255d257e2a8e6bd9ee6e535d738bdb8d53b48
Kessy Similien <simkessy@gmail.com>
2019-08-28 00:42:14 -0700

    basic init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+import { withFormik } from "formik";
+import yup from "yup";
 
-export default App;
+function App({ values, handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter Password"
+        value={values.passwords}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
+export default withFormik({
+  mapPropsToValues({ email = "asdfasf", password = "" }) {
+    return {
+      email: email,
+      password: password
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(App);
