39483595ef8b4179e0378bb28d0ba3fa50075079
Kessy Similien <simkessy@gmail.com>
2019-08-28 01:09:35 -0700

    Build basic form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
 import React from "react";
 
-import { withFormik } from "formik";
-import yup from "yup";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function App({ values, handleChange, handleSubmit }) {
+function App({ values, errors, touched, isSubmitting }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Enter email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Enter Password"
-        value={values.passwords}
-        onChange={handleChange}
-      />
-      <button>Submit</button>
-    </form>
+    <Form>
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Enter email" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Enter Password" />
+      </div>
+      <label>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+        Join our newsletter
+      </label>
+      <Field component="select" name="plan">
+        <option value="free">Free</option>
+        <option value="premium">Premium</option>
+      </Field>
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </Form>
   );
 }
 export default withFormik({
-  mapPropsToValues({ email = "asdfasf", password = "" }) {
+  mapPropsToValues({
+    email = "asdfasf",
+    password = "",
+    newsletter = false,
+    plan = "free"
+  }) {
     return {
-      email: email,
-      password: password
+      email,
+      password,
+      newsletter,
+      plan
     };
   },
-  handleSubmit(values) {
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required(),
+    password: Yup.string()
+      .min(4, "Needs more")
+      .required("Pass required")
+  }),
+  handleSubmit(values, { setErrors, setSubmitting, resetForm }) {
+    setTimeout(params => {
+      if (values.email === "simkessy@gmail.com") {
+        setErrors({
+          email: "Email is used"
+        });
+      } else {
+        resetForm();
+      }
+      setSubmitting(false);
+    }, 2000);
     console.log(values);
   }
 })(App);
